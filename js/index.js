da73c2c1809e7b94b210e466de6a99f0a1952ef2
Raikazu <vionwee3@gmail.com>
2024-10-29 21:14:51 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
 const title = document.querySelector('.title')
-const text1 = 'REREN I LOVE  YOU♥️'.split('')
-const text = 'MAU KAH KAMU BERSAMAKU MENGISI KEHIDUPAN YANG PAHIT INI'.split('')
+const text = 'REREN I LOVE  YOU♥️'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text1[index]}<span/>`
     title.innerHTML += `<span>${text[index]}<span/>`
   } else {
     title.innerHTML += `<span style='margin-right: 20px;'><span/>`
