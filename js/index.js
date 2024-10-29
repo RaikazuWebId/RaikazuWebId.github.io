cf4a6e5835fc297568e2ea996a16fd841ebfb32c
Raikazu <vionwee3@gmail.com>
2024-10-29 20:13:12 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I LOVE REREN'.split('')
+const text = 'REREN I LOVE YOU♥️'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
