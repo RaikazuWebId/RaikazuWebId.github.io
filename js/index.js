d86ae22c0074ebb996f8d4742d4e30fc2a3d888b
Raikazu <vionwee3@gmail.com>
2025-01-14 12:55:17 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something intan😍'.split('')
+const text = 'I Have Something intan LuvLuv🤍'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
